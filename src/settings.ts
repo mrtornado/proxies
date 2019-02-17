1d17f87a822837238a36c4c70c58749aa08e6374
Toro <toro@Torel.local>
2019-02-17 08:10:54 +0200

    new

--- src/settings.ts
+++ src/settings.ts
@@ -4,22 +4,22 @@
  This is the link to your domain...it redirects the user to your myaccount page.
  It is used on the site on buttons like: Buy now, Order now, Members, Buy HTTP proxies, etc.
 */
-export const SITE_URL = "https://site.url.goes.here";
+export const SITE_URL = "https://www.buy-proxy.net";
 
 /*
  Your site name - this appears in the footer
 */
-export const SITE_NAME = "Site name goes here";
+export const SITE_NAME = "Buy Proxy";
 
 /*
  The name of you site that appears in Terms of service section - it can be you domain, your company name, etc.
 */
-export const TOS_SITE_NAME = 'whatever-you-need';
+export const TOS_SITE_NAME = 'Buy-Proxy.net';
 
 /*
  The date when the terms of service where last updated.
 */
-export const TOS_LAST_UPDATE = 'Apr-27-2018';
+export const TOS_LAST_UPDATE = 'Feb-13-2019';
 
 /*
  This is the link of your chat widget - it can be found on your tawk to account
